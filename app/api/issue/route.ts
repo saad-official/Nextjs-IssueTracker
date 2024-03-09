c3e6eff2fa31abc1e4472e0d38c5e1a66b74cf8b
Sadi07 <saad.khan@zortik.com>
2024-03-09 09:15:59 +0500

    build an api for creating issue

---
+++ app/api/issue/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+import { z } from "zod";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validartion = createIssueSchema.safeParse(body);
+
+  if (!validartion.success)
+    return NextResponse.json(validartion.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
