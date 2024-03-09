f2762f6c1dc1c47764b0fce5ed5077a30cf25aea
Sadi07 <saad.khan@zortik.com>
2024-03-09 19:20:27 +0500

    show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import { Button } from '@radix-ui/themes'
+"use client";
+import React from "react";
+import { Button, Table } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import Link from "next/link";
 
-const IssuePage = () => {
+// eslint-disable-next-line @next/next/no-async-client-component
+const IssuePage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
-    <div><Button>New Issue</Button></div>
-  )
-}
+    <div>
+      <div className="mb-5">
+        <Button>
+          <Link href="/new">New Issue</Link>
+        </Button>
+      </div>
 
-export default IssuePage
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.ColumnHeaderCell>{issue.title}</Table.ColumnHeaderCell>
+
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                {issue.status}
+              </Table.ColumnHeaderCell>
+
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
+};
+
+export default IssuePage;
