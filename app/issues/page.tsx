0bdd717d4756e76a34e33f1bb4b3d3be99de2bdc
Sadi07 <saad.khan@zortik.com>
2024-03-13 09:49:11 +0500

    build a style link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import Link from "next/link";
+import Link from "../components/Link";
 import IssueStatusBadge from "../components/issueStatusBadge";
 import delay from "delay";
 import IssueAction from "./IssueAction";
